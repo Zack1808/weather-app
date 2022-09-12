b3f8e73bd6f5c60a5de199ef2061816163ed49e2
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-12 20:35:36 +0200

    Added unicons icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 // Creating the App component
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <h1 className='text-2xl font-bold text-red-500'>Hello World</h1>
+            <UilReact />
+        </div>
     )
 };
 
