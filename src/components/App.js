116459f3ad17e100b358e44f334519c5d60c6e6a
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-13 07:35:15 +0200

    Created the TimeAndLocation component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react';
 // Importing the costume made components
 import TopButtons from './TopButtons';
 import Inputs from './Inputs';
+import TimeAndLocation from './TimeAndLocation';
 
 // Creating the App component
 const App = () => {
@@ -11,6 +12,8 @@ const App = () => {
         <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
             <TopButtons />
             <Inputs />
+            
+            <TimeAndLocation />
         </div>
     )
 };
