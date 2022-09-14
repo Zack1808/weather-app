e4d2803c86b6fcc5a47d0627d43f092f423b0185
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-14 19:56:13 +0200

    Created and styled the search component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // Importing costume made components
 import Header from './Header';
+import Search from './Search'
 
 // Importing the style file
 import '../css/App.css';
@@ -12,6 +13,9 @@ const App = () => {
         <div className='container'>
             <div className="display-weather">
                 <Header />
+                <div className="information-collection">
+                    <Search />
+                </div>
             </div>
         </div>
     )
---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { UilSearch } from '@iconscout/react-unicons';
+
+// Importing the style file
+import '../css/Search.css';
+
+// Creating the search component
+const Search = () => {
+    return (
+        <div className="search">
+            <UilSearch className="icon" />
+            <input type="text" placeholder='Search for cities...' />
+        </div>
+    )
+};
+
+export default Search;
