fa77015405560765ff12c4e0ac8f332a363da674
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-02-09 20:03:40 +0100

    Restructuring the Weather app. Created NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
 
-// Importing costume made components
-import Header from './Header';
-import InformationCollection from './InformationCollection';
-import Details from './Details';
-import WeatherInfo from './WeatherInfo';
-import Footer from './Footer'
-import Loader  from './Loader';
+// Importing the costume components
+import NavBar from "./NavBar";
 
 // Importing the style file
-import '../css/App.css';
-
-// Importing the api
-import fetchWeather from '../api/fetchWeather';
-
-// Importing helper functions
-import { fetchLocation } from '../helpers/formaters'
-import { changeBg } from '../helpers/formaters';
+import "../css/App.css";
 
 // Creating the App component
 const App = () => {
-
-    const [query, setQuery] = useState({q: "Velika Gorica"});
-    const [units, setUnits] = useState({units: "metric"});
-    const [weather, setWeather] = useState(null);
-
-    useEffect(() => {
-        fetchLocation(setQuery)
-    }, [])
-
-    useEffect(() => {
-        fetch(query)
-    }, [query, units])
-
-    // Fetching the weather information
-    const fetch = async(query) => {
-        const weatherInfo = await fetchWeather({...query, ...units});
-        setWeather(weatherInfo);
-    }
-
-    return (
-        <div className={`container ${changeBg(weather, units)}`}>
-            <div className="display-weather">
-                <Header setQuery={setQuery} />
-                <InformationCollection setQuery={setQuery} setUnits={setUnits} />
-                {
-                    weather ? 
-                    (
-                        <>
-                            <Details weather={weather} />
-                            <WeatherInfo weather={weather} />
-                            <Footer weather={weather} />
-                        </>
-                    ) 
-                    : 
-                    (
-                        <Loader />
-                    )
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="app-container">
+      <div className="navigation">
+        <NavBar />
+      </div>
+    </div>
+  );
 };
 
+// Exporting the App component
 export default App;
