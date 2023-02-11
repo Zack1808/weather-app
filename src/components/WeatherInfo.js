016616117375946ca197d1a7f052a6db02240812
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-02-11 23:14:15 +0100

    Added Weather info

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+  UilTemperatureMinus,
+  UilTemperaturePlus,
+} from "@iconscout/react-unicons";
 
 // Importing the style file
 import "../css/WeatherInfo.css";
@@ -13,6 +22,33 @@ const WeatherInfo = () => {
           <h2>Clear</h2>
         </div>
         <div className="temp">4°</div>
+      </div>
+      <div className="weather-info">
+        <div className="weather">
+          <span>
+            <UilTemperature /> Real feel: 1°
+          </span>
+          <span>
+            <UilTear /> Humidity: 64%
+          </span>
+          <span>
+            <UilWind /> Wind: 1km/h
+          </span>
+        </div>
+        <div className="temp-day">
+          <span>
+            <UilSun /> Sunrise: 7:05AM
+          </span>
+          <span>
+            <UilTemperatureMinus /> Lowest: -2°
+          </span>
+          <span>
+            <UilTemperaturePlus /> Highest: 1°
+          </span>
+          <span>
+            <UilSunset /> Sunset: 5:15PM
+          </span>
+        </div>
       </div>
     </div>
   );
